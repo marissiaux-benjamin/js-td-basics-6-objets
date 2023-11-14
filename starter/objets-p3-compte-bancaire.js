bf5e85ab135d9f8f71a84c148263c2f4c940f7a2
benjamin <benjamin.marissiaux@student.hepl.be>
2023-11-14 19:34:20 +0100

    objets-p3-compte-bancaire.js

--- starter/objets-p3-compte-bancaire.js
+++ starter/objets-p3-compte-bancaire.js
@@ -12,3 +12,21 @@ OBJETS - PRÉPA 3 : Modélisation d'un compte bancaire
    - puis le débiter de 80,
    - et enfin afficher de nouveau sa description.
 */
+const compte = {
+    titulaire : "Alex",
+    solde : 0,
+    crediter(montant){
+        return  this.solde = this.solde + montant;
+    },
+    decrire() {
+        return `${this.titulaire} a présentement ${this.solde} sur son compte en banque.`;
+    }
+}
+
+
+console.log(`${compte.decrire()}`);
+console.log(`Alex a été crédité de ${compte.crediter(250)}€.`);
+console.log(compte.decrire());
+console.log(`Alex a été débité de ${compte.crediter(-80)}€.`);
+console.log(compte.decrire());
+
