28ca2d65a25100155f95b4d328196261ed2f70a8
benjamin <benjamin.marissiaux@student.hepl.be>
2023-11-14 18:32:05 +0100

    objets-p2-chien.js

--- starter/objets-p2-chien.js
+++ starter/objets-p2-chien.js
@@ -9,3 +9,18 @@ OBJETS - PRÉPA 2 : Modélisation d'un chien
 3. Affiche dans la console le message "Tiens, un chat ! … aboie : …"
    (où … est le nom du chien et … utilise la méthode aboyer du chien pour le faire aboyer)
 */
+const chien = {
+    nom : 'Tobby',
+    race : 'BOrder Collie',
+    couleur : 'bleu merle tricolore',
+    grogner(){
+        return "Grrrr!! Grrr !!";
+    },
+    aboyer(){
+        return "Wah!! Wah !!";
+    }
+}
+
+console.log(`${chien.nom} est un ${chien.race} de couleur ${chien.couleur}`);
+
+console.log(`Tiens, un chat ! ${this.nom} aboie : ${chien.aboyer()}`);
