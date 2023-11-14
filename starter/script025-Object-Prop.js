3efe6f64186877d00b76d4d35c3b202d5a9d2ef8
benjamin <benjamin.marissiaux@student.hepl.be>
2023-11-14 19:57:12 +0100

    script025-Object-Prop.js

--- starter/script025-Object-Prop.js
+++ starter/script025-Object-Prop.js
@@ -9,11 +9,11 @@
 // Crée un objet 'moi' qui te présente et contient les propriétés suivantes :
 // nom, prenom, age, profession, nationalite
 const moi = {
-    nom: 'VotreNom',
-    prenom: 'VotrePrenom',
-    age: 25,
+    nom: 'Maru',
+    prenom: 'Ben',
+    age: 22,
     profession: 'Étudiant',
-    nationalite: 'Française'
+    nationalite: 'Belge'
 };
 
 // Affiche l'objet que tu viens de créer dans la console
@@ -24,9 +24,14 @@ console.log(moi);
 // Affiche dans la console : "Je m'appelle … …, j'ai … ans, je suis … et je suis …"
 // en allant à chaque fois rechercher les propriétés dans l'objet 'moi' que tu viens de créer
 
+console.log(`Je m'appelle ${['Ben']} ${['Maru']}, j'ai ${['22']} ans, je suis ${['Étudiant']} et je suis ${['Belge']}`);
+
 // 2°) Deuxième méthode : l'opérateur . (point)
 // Affiche dans la console : "Je m'appelle … …, j'ai … ans, je suis … et je suis …"
 // en allant à chaque fois rechercher les propriétés dans l'objet 'moi' que tu viens de créer
+
+console.log(`Je m'appelle ${moi.prenom} ${moi.nom}, j'ai ${moi.age} ans, je suis ${moi.profession} et je suis ${moi.nationalite}`);
+
 
 /* 3. Modification des propriétés de l'objet */
 // Trois années se sont écoulées, tu es sorti(e) de l'école, tu es à présent 'Web Developer'
@@ -35,7 +40,11 @@ console.log(moi);
 // Affiche à nouveau ta description dans la console :
 // "Je m'appelle … …, j'ai  … ans, je suis à présent un … (métier) … (nationalité) et je suis … (état civil)"
 // en allant à chaque fois rechercher les propriétés dans l'objet 'moi' que tu viens de créer
-
+moi.age = 25;
+moi.profession = 'Web Developer';
+moi.etatCivil = 'marié';
+console.log(moi);
+console.log(`Je m'appelle ${moi.prenom} ${moi.nom}, j'ai ${moi.age} ans, je suis à présent ${moi.profession} ${moi.nationalite} et je suis ${moi.etatCivil}.`);
 
 /*
 EXERCICE 4 :
@@ -50,3 +59,28 @@ EXERCICE 4 :
 - Ajoute une propriété "prix" avec la valeur 2.5
 - Affiche "Mon stylo coûte … euros"
 */
+const stylo = {
+    type : 'bille',
+    couleur : 'bleu',
+    marque : 'bic'
+}
+
+console.log(stylo.type);
+console.log(stylo.couleur);
+console.log(stylo.marque);
+
+console.log(`J'écris avec un stylo ${stylo.type} ${stylo.couleur} de marque ${stylo.marque}`);
+
+stylo.couleur = 'rouge';
+console.log(`J'écris avec un stylo ${stylo.type} ${stylo.couleur} de marque ${stylo.marque}`);
+
+stylo.prix = 2.5;
+console.log(`Mon stylo coûte ${stylo.prix} euros`);
+
+
+
+
+
+
+
+
